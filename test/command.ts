1138bc46739dea9ac1ce1294a79aab48cebc32cc
Nin <darshan310268@gmail.com>
2021-08-19 13:42:34 +0530

    Update command.ts

--- test/command.ts
+++ test/command.ts
@@ -1,4 +1,4 @@
-const client = require('path to index file');
+import { Client } from "discord.js";
 
 module.exports = {
     name: 'test',
@@ -6,6 +6,12 @@ module.exports = {
     hidden: true,
     description: 'test command',
     usage: '<test> [test]',
+    
+    /**
+     * @param {Client} bot
+     * @param {String} msg
+     * @param msg message
+     */
 
     run: async(msg) => {
         msg.reply('Hi! :) this is a test!')
