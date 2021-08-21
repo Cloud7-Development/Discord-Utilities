a6a230f226a9aeccbb6e3c5756f795cb4b86286b
Zapdos <71247896+Zapd0s@users.noreply.github.com>
2021-08-21 11:09:16 +0530

    Update purge.js
    Add specific error messages

--- commands/purge.js
+++ commands/purge.js
@@ -6,9 +6,9 @@ module.exports = {
 
     run: async(message, args) => {
         if(!message.member.permissions.has('MANAGE_MESSAGES'))
-        return message.react('⚠️');
+        return message.channel.send('You need `Manage Message` permission to use the purge command.');
 
-        if(!args[0]) return message.react('⚠️');
+        if(!args[0]) return message.channel.send('You need to specify the number of messages to purge!');
         if(isNaN(args[0])) return message.react('⚠️');
         if(args[0] > 99 || args[0] < 1) return message.react('⚠️');
         
