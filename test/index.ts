028f91b4b265b75f47d1ac4765659a564ba46267
Nin <77528189+Ninn08@users.noreply.github.com>
2021-09-27 10:08:14 +0530

    🧨 commit

--- test/index.ts
+++ test/index.ts
@@ -4,10 +4,13 @@ const client = new Client({
     intents: 32767
 });
 
+client.commands = new Collection();
+// command handler goes here
+
 client.on('messageCreate', async(message) => {
     if(message.content === 'hi'){
         message.reply('Hello!')
     }
 });
 
-client.login('hahahahahahahahhahahahahhahahaha.lolololol.kekboom');
+client.login('hahahahahahahahhahahahahhahaha.lolololol.kekboom');
